1473076bc37c527dd56fafac0b78396ea8cd7280
Deepak Negi <DNegi@professionalaccess.com>
2017-01-10 18:47:35 +0530

    Changes

--- src/components/megamenu.js
+++ src/components/megamenu.js
@@ -1,7 +1,18 @@
 import React , { Component } from 'react';
-
+import store from './reducers/index'
+import axios from 'axios'
 class  MegaMenu extends Component {
 
+componentWillMount(){
+	console.log(axios);
+
+	axios.get('/user?ID=12345').then(function (response) {
+		    console.log(response);
+  		}).catch(function (error) {
+    console.log(error);
+  });
+
+}
 render(){ 
 
 	return (
--- src/mystore.js
+++ src/mystore.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 //import { Provider } from 'react-redux';
 //import { createStore, applyMiddleware } from 'redux';
 
-import store from './reducers/index'
 import App from './components/app';
 //import reducers from './reducers';
 import MegaMenu from './components/megamenu'
